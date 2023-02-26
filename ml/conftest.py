daa683922993a418c49de61703800f3f84536bfb
Mary <maryamfallah1994@gmail.com>
2023-02-26 10:53:41 -0500

    adds tests and ci

---
+++ ml/conftest.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+load_dotenv()
+"""
+Loading shared scripts for unittest
+This prevents throwing errors when unit test is running without logging code mounted in utils/
+"""
+test_path = os.path.abspath("../shared_utils/unittest")
+sys.path.insert(0, test_path)
