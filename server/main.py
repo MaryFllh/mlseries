f03a37d0fda470f1f92f6dbe7f0b35a4ceeddbd2
Mary <maryamfallah1994@gmail.com>
2023-03-04 20:05:36 -0500

    need to mock request to ml for unittest

--- server/main.py
+++ server/main.py
@@ -3,6 +3,8 @@ import requests
 
 from fastapi import FastAPI, HTTPException, status
 from config import Config
+from entities.review import Review
+from utils.logger import logger
 
 app = FastAPI()
 config = Config()
@@ -10,7 +12,7 @@ config = Config()
 
 
 @app.post("/review_sentiment/", status_code=status.HTTP_200_OK)
-def add_review(review: str):
+def add_review(review: Review):
     """
     This endpoint receives reviews, creates an appropriate
     payload and makes a post request to ml's endpoint for a
@@ -27,12 +29,11 @@ def add_review(review: str):
         HTTPException: if the input is a sequence of numbers
     """
     endpoint = f"{config.ml_base_uri}/prediction_job/"
-    import pdb; pdb.set_trace()
-    if re.findall(r'^\-?[1-9][0-9]*$', review):
+    if re.findall(r'^\-?[1-9][0-9]*$', review.text):
         raise HTTPException(status_code=422, detail="Review cannot be a number")
-
+    
     payload = {
-        "text": review,
+        "text": review.text,
     }
     resp = requests.post(endpoint, json=payload)
     return resp.json()
