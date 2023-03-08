9949d58acbb54701cec94600d44ca5c9f9ed885a
Mary <maryamfallah1994@gmail.com>
2023-03-08 06:35:57 -0500

    flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics

--- server/main.py
+++ server/main.py
@@ -4,11 +4,9 @@ import requests
 from fastapi import FastAPI, HTTPException, status
 from config import Config
 from entities.review import Review
-from utils.logger import logger
 
 app = FastAPI()
 config = Config()
-
 
 
 @app.post("/review_sentiment/", status_code=status.HTTP_200_OK)
@@ -17,24 +15,23 @@ def add_review(review: Review):
     This endpoint receives reviews, creates an appropriate
     payload and makes a post request to ml's endpoint for a
     prediction on the input
-    
+
     Args:
         review(Review): the input should conform to the Review model
-    
+
     Returns:
         response(dict): response from ml's endpoint which includes
                         prediction details
-    
+
     Raises:
         HTTPException: if the input is a sequence of numbers
     """
     endpoint = f"{config.ml_base_uri}/prediction_job/"
-    if re.findall(r'^\-?[1-9][0-9]*$', review.text):
+    if re.findall(r"^\-?[1-9][0-9]*$", review.text):
         raise HTTPException(status_code=422, detail="Review cannot be a number")
-    
+
     payload = {
         "text": review.text,
     }
     resp = requests.post(endpoint, json=payload)
     return resp.json()
-
--- server/unittests/test_server.py
+++ server/unittests/test_server.py
@@ -1,19 +1,44 @@
-import os
+import pytest
 
 from fastapi import HTTPException
 from fastapi.testclient import TestClient
+from pydantic import ValidationError
+from requests_mock import Adapter
 
-from main import app
-
+from entities.review import Review
+from main import app, add_review
+from config import Config
 
 
 client = TestClient(app)
+config = Config()
 
-def test_add_review():
+
+def test_add_review(requests_mock: Adapter):
+
     review1 = 123
-    response = client.post("/review_sentiment/", json={"review": review1})
-    assert response.status_code == 422
-    
-    # review2 = "very pleasant. highly recommend"
-    # response = client.post("/review_sentiment/", json={"review": str(review2)})
-    # assert response.status_code == 201
+    with pytest.raises(ValidationError) as exc_info:
+        add_review(Review(text=review1))
+    assert exc_info.value.errors() == [
+        {
+            "loc": ("text",),
+            "msg": "min_length:4",
+            "type": "value_error.any_str.min_length",
+            "ctx": {"limit_value": 4},
+        }
+    ]
+
+    review2 = 123458303
+    with pytest.raises(HTTPException) as exc_info:
+        add_review(Review(text=review2))
+    assert exc_info.value.status_code == 422
+    assert exc_info.value.detail == "Review cannot be a number"
+
+    adapter = requests_mock.post(
+        f"{config.ml_base_uri}/prediction_job/",
+        json={"prediction": "positive", "prediction_score": 80},
+    )
+    review3 = "very pleasant. highly recommend"
+    add_review(Review(text=review3))
+    assert adapter.last_request.json() == {"text": review3}
+    assert adapter.call_count == 1
