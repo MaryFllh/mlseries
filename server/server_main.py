e75069c953c7a184c726921d7d7f1f1cb28b1106
Mary <maryamfallah1994@gmail.com>
2023-03-31 10:56:36 -0400

    removed hooks

--- server/server_main.py
+++ server/server_main.py
@@ -2,8 +2,8 @@ import re
 
 import requests
 from config import Config
-from fastapi import FastAPI, HTTPException, status
 from entities.review import Review
+from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
 config = Config()
